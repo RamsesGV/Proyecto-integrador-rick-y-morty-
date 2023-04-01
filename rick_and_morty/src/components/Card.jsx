9995a31c737d5fc238913277ef9d5d502d57f883
RamsesGV <senkustone15@gmail.com>
2023-04-01 07:47:04 -0600

    1

---
+++ rick_and_morty/src/components/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card({name,species,gender,image,onClose}) {
+   return (
+      <div>
+         
+         <button onClick={onClose}>X</button>
+         <h2>{name}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin.name}</h2>
+         <img src={image} alt='Personaje' /> 
+         
+      </div>
+   );
+}
