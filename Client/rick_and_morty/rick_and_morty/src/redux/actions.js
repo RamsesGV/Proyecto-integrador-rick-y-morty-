75d862e8dcd661225f68667c65294df9ab97b819
RamsesGV <senkustone15@gmail.com>
2023-04-25 07:36:08 -0600

    actualizacion options async/await

--- Client/rick_and_morty/rick_and_morty/src/redux/actions.js
+++ Client/rick_and_morty/rick_and_morty/src/redux/actions.js
@@ -6,32 +6,47 @@ export const ORDER = 'ORDER'
 
 
 export const addFav = (character) => {
-  // return { type: ADD_FAV, payload: character }
-  const endpoint = 'http://localhost:3001/rickandmorty/fav';
-  return (dispatch) => {
-     axios.post(endpoint, character)
-     .then(({ data }) => {
-          return dispatch({
-              type: ADD_FAVORITE,
-              payload: data,
-        });
-     });
-  };
+const endpoint = 'http://localhost:3001/rickandmorty/fav';
+return async (dispatch) => {
+    try {
+        const {data} = await  axios.post(endpoint, character)
+        if(!data.length) throw Error('no hay favoritos')
+
+        return dispatch({
+            type: ADD_FAVORITE,
+            payload: data,
+            });
+
+        } catch (error) {
+        console.log(error.message);
+        }
+
+    }
+        
+    
 };
 
 
 export const removeFav = (id) => {
-  // return { type: REMOVE_FAV, payload: id }
-  const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
-  return (dispatch) => {
-      axios.delete(endpoint)
-      .then(({ data }) => {
-          return dispatch({
-              type: REMOVE_FAVORITE,
-              payload: data,
-          });
-      });
-  };
+
+const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
+return async (dispatch) => {
+    try {
+
+        const { data } = await axios.delete(endpoint)
+    
+        return dispatch({
+            type: REMOVE_FAVORITE,
+            payload: data,
+        });
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+    
+    
+    
+    };
 }
 
 export const filterCards = (gender) => { 
